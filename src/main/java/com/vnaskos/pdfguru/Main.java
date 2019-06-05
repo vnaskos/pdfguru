1f8166ea6541a0a433b878a8936f036dd82b4ea1
vnaskos <vnaskos@gmail.com>
2019-06-06 01:31:22 +0300

    Revert styling changes

--- src/main/java/com/vnaskos/pdfguru/Main.java
+++ src/main/java/com/vnaskos/pdfguru/Main.java
@@ -2,9 +2,17 @@ package com.vnaskos.pdfguru;
 
 import com.vnaskos.pdfguru.ui.PDFGuru;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String... args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(PDFGuru.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
         java.awt.EventQueue.invokeLater(() -> new PDFGuru().setVisible(true));
     }
 }
